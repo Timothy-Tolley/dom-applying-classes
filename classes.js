b3282aec26c767adec24b6895541ebe9fbed2235
Tim Tolley <Tim@Tims-MacBook-Pro.local>
2017-10-20 16:12:29 +1300

    second circle Green

--- classes.js
+++ classes.js
@@ -1,12 +1,13 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -16,6 +17,14 @@ function one () {
 
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
+}
+
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
 }
 
 // CREATE FUNCTION two HERE
